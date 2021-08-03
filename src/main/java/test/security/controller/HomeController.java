cf3001b0073ec157d937eba7b32e231de81dd0b7
goosebumki <qjsro742@gmail.com>
2021-08-03 20:39:46 +0900

    git test commit

--- src/main/java/test/security/controller/HomeController.java
+++ src/main/java/test/security/controller/HomeController.java
@@ -2,11 +2,18 @@ package test.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @ResponseBody
+    @GetMapping("/test")
+    public String test() {
+        return "test";
     }
 }
